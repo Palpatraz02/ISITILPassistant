78946054f82ab67610602a149769adac0180963e
Palpatraz02 <33456227+Palpatraz02@users.noreply.github.com>
2018-09-15 18:45:18 +0200

    Update ISITILPassistant.py

--- ISITILPassistant.py
+++ ISITILPassistant.py
@@ -1,11 +1,28 @@
 import Funzioni
 
 def comandi(domanda):
-    key_words=[]
-    if domanda.find("non"):
-        key_words.append("non")
-    if Funzioni.memoria(domanda)==-1: Funzioni.impara(input("Non ho mai sentito questa domanda cosa devo riponere?\n ->"))
-    else: print(Funzioni.memoria(domanda))
+    provabile_frase_negativa=False
+    azione_promaria=""
+    parametri=[]
+    if domanda.find("non")!=-1:
+        provabile_frase_negativa=True
+    elif domanda.find("informazioni")!=-1 or domanda.find("informazione")!=-1:
+        azione_promaria="informazioni"
+    elif domanda.find("professori")!=-1 or domanda.find("professore")!=-1:
+        parametri.append("professori")
+
+    if provabile_frase_negativa==False:
+        if azione_promaria=="informazioni":
+            if len(parametri)!=0:
+                for x in parametri:
+                    if x == "professori":
+                        print("Alloara hai chiesto informazzioni sui professori")
+            else:
+                print("Su cosa vuoi che ti informi")
+
+
+    #if Funzioni.memoria(domanda)==-1: Funzioni.impara(input("Non ho mai sentito questa domanda cosa devo riponere?\n ->"))
+    #else: print(Funzioni.memoria(domanda))
 
 while True:
     comandi(Funzioni.rimuovi_spazzi_non_necessari(input("Di cosa hai bisogno?\n -> ")))
