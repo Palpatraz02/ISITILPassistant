943cdc4c0b467469b43e11cad37d52c0c1e1d126
Samuele Lolli <39225931+Samuele1414@users.noreply.github.com>
2018-09-15 11:06:37 +0200

    Modifica idea di modo di sviluppare
    Prova a fare le funzioni come verrebbe

--- ISITILPassistant.py
+++ ISITILPassistant.py
@@ -1,1 +1,9 @@
 print("linea da cancellare")
+domanda = input("Di cosa hai bisogno?  /n/n Indicazioni, informazioni, aiuto o altro")
+
+if (domanda == "indicazioni") or (domanda == "Indicazioni"):
+  print("Che tipo di indicazioni hai bisogno?")
+  indicazionidomanda = input("Professori, classi, materiale didattico")
+  
+  
+  
