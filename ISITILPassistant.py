32c6dea736b4ba07e97e5d7b3a3b85b6d0a3d2f8
Palpatraz02 <33456227+Palpatraz02@users.noreply.github.com>
2018-09-16 11:08:08 +0200

    Update ISITILPassistant.py

--- ISITILPassistant.py
+++ ISITILPassistant.py
@@ -1,6 +1,10 @@
 import Funzioni
 
 def comandi(domanda):
+    lista_materie=["italiano","matematica","francese","inglese","chimica","fisica","diritto"]
+    lista_classi_n=[n for n in range(1,6)]
+    lista_classi_n = ["prima","seconda","terza","quarta","quinta"]
+
     probabile_frase_negativa=False
     domanda_si_no = False
     azione_sempre_vera=""
@@ -16,15 +20,25 @@ def comandi(domanda):
         azione_primaria="informazioni"
     if domanda.find("professori")!=-1 or domanda.find("professore")!=-1:
         parametri.append("professori")
+        for materia in lista_materie:
+            if domanda.find(materia) != -1:
+                parametri.append(materia)
 
+
+    find=False
     if probabile_frase_negativa==False:
         if domanda_si_no==False:
             if azione_sempre_vera=="ciao":
                 print("Ciao!!")
             if azione_primaria=="informazioni":
                 if len(parametri)!=0:
-                    for x in parametri:
-                        if x == "professori":
+                    if parametri[0] == "professori":
+                        if len(parametri) >= 2:
+                            for materia in lista_materie:
+                                if parametri[1]==materia:
+                                    print(f"Mi dispice ma sono nuovo non so niente sui professori di {materia}")
+                                    find=True
+                        if find==False:
                             print("Alloara hai chiesto informazzioni sui professori")
                 else:
                     print("Su cosa vuoi che ti informi")
